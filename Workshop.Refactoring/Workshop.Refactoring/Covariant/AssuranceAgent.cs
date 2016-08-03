528879e77b1794661658c424c222fd9a9512b469
Mehran <mehrandvd@yahoo.com>
2016-08-04 01:04:56 +0430

    Refactoring folders

--- Workshop.Refactoring/Workshop.Refactoring/Covariant/AssuranceAgent.cs
+++ Workshop.Refactoring/Workshop.Refactoring/Covariant/AssuranceAgent.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Workshop.Refactoring.Step2;
+using Workshop.Refactoring;
 
 namespace Workshop.Refactoring.Step3
 {
